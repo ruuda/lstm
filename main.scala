7e68ddebaaa51dfb311e0dc107de2d3ae46a4319
Ruud van Asseldonk <dev@veniogames.com>
2017-05-22 16:40:28 +0200

    Define vector with some operations
    Ugh, Scala is still quite verbose for this kind of thing. Maybe I should
    have chosen Haskell instead. But for now this will suffice.

--- main.scala
+++ main.scala
@@ -1,3 +1,21 @@
+
+case class Vector(xs: Seq[Float]) {
+  def +(that: Vector): Vector = {
+    val Vector(ys) = that
+    require(xs.length == ys.length)
+    val zs = xs.zip(ys).map { case (x, y) => x + y }
+    Vector(zs)
+  }
+
+  def dot(that: Vector): Float = {
+    val Vector(ys) = that
+    require(xs.length == ys.length)
+    xs.zip(ys).foldLeft(0.0f) { case (acc, (x, y)) => acc + x * y }
+  }
+}
+
+case class Matrix(xss: Seq[Seq[Float]])
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hi")
